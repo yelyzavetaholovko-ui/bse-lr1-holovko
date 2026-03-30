27a6ca13a32973b43f5ad7a5766cc062b7e669a9
Yelyzaveta Holovko <yelyzaveta.holovko@nure.ua>
2026-03-30 23:21:26 +0300

    Додано базовий код у Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BseLr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторна робота №1");
+            Console.WriteLine("Виконала: Головко Єлизавета, група ПЗПІ-25-3");
+        }
+    }
+}
